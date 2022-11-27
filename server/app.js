73ef45b518062c2fba618c41f226b0072c50f545
Osvaldo Rojas G <o.rojas.g.2022@gmail.com>
2022-11-27 03:59:41 -0300

    api

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
 /**
@@ -10,10 +12,18 @@ const app = express();
 /**
  * Configure BodyParser
  */
+app.use(bodyParser.urlencoded({extends: true}));
+app.use(bodyParser.json());
+
+/**
+ * Configure Statics Folders
+ */
+app.use(express.static("uploads"));
 
 /**
  * COnfigure Header HTTP - CORS
  */
+app.use(cors());
 
 /**
  * Configure Routes
